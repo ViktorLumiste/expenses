c13708f9998aa91c4e59a7a10270b39c7ea81a21
ViktorLumiste <viktor.lumiste@gmail.com>
2023-04-05 14:43:42 +0300

    Split components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,14 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
 function ExpenseItem(props){
-    console.log(props)
+
+
 
     return (
         <div className="expense-item">
-            <div>{props.expenseData.date.toString()}</div>
+            <ExpenseDate date={props.expenseData.date}></ExpenseDate>
             <div className="expense-item__description">
-                <h2>{props.expenseData.title}</h2>
-                <div className="expense-item__price">{props.expenseData.price}</div>
+                <h2></h2>
             </div>
         </div>
     )
