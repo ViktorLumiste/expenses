0299151ffc0a65f9aa580483009a34085425b792
ViktorLumiste <viktor.lumiste@gmail.com>
2023-04-05 14:53:48 +0300

    Added style to dates

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -8,7 +8,8 @@ function ExpenseItem(props){
         <div className="expense-item">
             <ExpenseDate date={props.expenseData.date}></ExpenseDate>
             <div className="expense-item__description">
-                <h2></h2>
+                <h2>{props.expenseData.title}</h2>
+                <div className='expense-item__price'>{props.expenseData.price}</div>
             </div>
         </div>
     )
