cc0eee468c2fa5f7c080c9e45f311d2b71bf156c
ViktorLumiste <viktor.lumiste@gmail.com>
2023-04-05 14:16:55 +0300

    Added data to components

--- src/App.js
+++ src/App.js
@@ -4,11 +4,25 @@ import './App.css';
 import ExpenseItem  from "./components/ExpenseItem";
 
 function App() {
-  return (
-    <div className="App">
-      <ExpenseItem></ExpenseItem>
-    </div>
-  );
-}
+    const expenses = [
+        {
+            date: new Date(2023, 0, 10),
+            title: 'New book',
+            price: 30.99
+        },
+        {
+            date: new Date(2023, 0, 10),
+            title: 'New Jeans',
+            price: 99.99
+        }
+    ]
 
+    return (
+        <div className="App">
+            <ExpenseItem expenseData={expenses[0]}></ExpenseItem>
+            <ExpenseItem expenseData={expenses[1]}></ExpenseItem>
+        </div>
+    );
+
+}
 export default App;
