18a992eda82de3a248f06fa6cdd855d1f9989a21
ViktorLumiste <viktor.lumiste@gmail.com>
2023-04-12 11:45:10 +0300

    Adding component events

--- src/Expenses/ExpenseItem.js
+++ src/Expenses/ExpenseItem.js
@@ -1,15 +1,23 @@
+import React, {useState} from 'react';
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate'
 const  ExpenseItem = (props) =>{
-    console.log(props)
+    const [title, setTitle] = useState(props.expenseData.expenseData.title)
+    const clickHandler = () =>{
+        setTitle('Updated!')
+        console.log(title)
+    }
     return (
+        <>
         <div className="expense-item">
             <ExpenseDate date={props.expenseData.expenseData.date}></ExpenseDate>
             <div className="expense-item__description">
                 <h2>{props.expenseData.expenseData.title}</h2>
                 <div className='expense-item__price'>{props.expenseData.expenseData.price}</div>
             </div>
+            <button onClick={clickHandler}>Change Title</button>
         </div>
+        </>
     )
 }
 
